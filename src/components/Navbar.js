7951a5279fb2b2f2b03ce0e936073b28bc8290d5
Uluk Abylbekov <uluk_abylbekov@uluks-macbook-air.local>
2021-02-01 10:51:04 +0000

    created Nvabr component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar bg-blue-100 flex">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;

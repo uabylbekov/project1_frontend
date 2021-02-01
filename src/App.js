4cc4be54ff9e018cbd47eff0f8a1656702b0ab05
Uluk Abylbekov <uluk_abylbekov@uluks-macbook-air.local>
2021-02-01 10:51:23 +0000

    added Navbar componnent to root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div> Hello Project1</div>
+  return <Navbar />;
 }
 
 export default App;

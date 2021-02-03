0b80519538c6f33a205346f8e3194d9610bf31ec
Uluk Abylbekov <uluk_abylbekov@uluks-macbook-air.local>
2021-02-03 05:15:33 +0000

    React routers adde

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Searchbar from '../elements/Searchbar'
 
 function Navbar() {
   return (
-    <div className="navbar bg-blue-100 flex">
-      <div></div>
-      <div></div>
+    <div className="flex justify-between bg-white shadow h-12 px-5">
+      <div className="flex justify-between">
+        <Link className="pt-3" to="/">Logo</Link>
+        <Searchbar/>
+      </div>
+      <div className="flex justify-between pt-3">
+        <ul className="flex justify-between">
+          <li className="mr-5">
+            <Link to="/">log in</Link>
+          </li>
+          <li>
+            <Link to="/create">new post</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

478a8c1e0d4ddfa9072a28d337fec39864995932
Uluk Abylbekov <uluk_abylbekov@uluks-macbook-air.local>
2021-02-03 05:16:04 +0000

    Router views added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  return <Navbar />;
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route component={HomePage} path="/" exact />
+          <Route component={CreatePage} path="/create" exact />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

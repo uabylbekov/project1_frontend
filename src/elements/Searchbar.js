c398a0e50dcf0be3407662b008f26ebfc1d09a4f
Uluk Abylbekov <uluk_abylbekov@uluks-macbook-air.local>
2021-02-03 05:14:55 +0000

    Searchbar component added

---
+++ src/elements/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Searchbar() {
+	return (
+		<form className="pt-2">
+		  <input className="shadow-inner rounded-xl h-8 px-5 ml-5 bg-gray-100" type="search" placeholder="Search" aria-label="Search"/>
+		</form>
+	)
+}
+
+export default Searchbar
